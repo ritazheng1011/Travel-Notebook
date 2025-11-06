4a7d16b3f31b24b95a64f2c2ca64c4124fc2dcb3
Alyssa F <aiepillios1101@gmail.com>
2025-11-06 17:37:48 -0500

    Created JS files for page functionality + internalstorage

--- main.js
+++ main.js
@@ -1,13 +1,2 @@
-// We can worry about the JS towards the end
+// creating a new log
 
-// Saving a session ------>
-// const session = {
-//   screens: [],
-//   state: true,
-// };
-// session.screens.push({ name: "screenA", width: 450, height: 250 });
-
-// // Converting the JSON string with JSON.stringify()
-// // then saving with localStorage in the name of session
-
-// localStorage.setItem("session", JSON.stringify(session));
